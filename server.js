0fe7c8db32a85f0a187dbdd2382b05ba4da0b668
Tanner Holle <tannerholle96@gmail.com>
2025-10-30 11:17:06 -0600

    Revert fly.io

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const User = require('./models/User');
 const Bet = require('./models/Bet');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
